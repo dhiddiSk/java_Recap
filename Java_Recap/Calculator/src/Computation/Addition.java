98de1275aa0daf794bdf049b276fe1075c04f62c
Saikrishna <saikrishna.4369@gmail.com>
2023-01-22 19:50:23 +0530

    Update Addition.java

--- Java_Recap/Calculator/src/Computation/Addition.java
+++ Java_Recap/Calculator/src/Computation/Addition.java
@@ -3,10 +3,7 @@ package Computation;
 public class Addition implements Computation{
 
 	@Override
-	public double operation(double operand1, double operand2) {
-		
+	public double operation(double operand1, double operand2) {	
 		return (operand1 + operand2);
 	}
-
-
 }
